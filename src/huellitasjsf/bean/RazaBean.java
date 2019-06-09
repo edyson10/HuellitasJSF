c7439e2fa03ae2f9144de3cfb2c2ccc4d8a73203
edyson10 <edysonfabian@hotmail.com>
2019-06-09 00:20:07 -0500

    Por fin carga el combobox xD

--- src/huellitasjsf/bean/RazaBean.java
+++ src/huellitasjsf/bean/RazaBean.java
@@ -2,15 +2,17 @@ package huellitasjsf.bean;
 
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import huellitasjsf.model.DAO.RazaDAO;
 import huellitasjsf.model.entities.Raza;
 
-@ManagedBean
+@ManagedBean(name="razaBean")//agregue esto
 @SessionScoped
-public class RazaaBean {
-	
+public class RazaBean {
+	@ManagedProperty(value = "#{raza}")//agregue esto
 	private Raza raza = new Raza();
+	 @ManagedProperty(value = "#{combo}")//agregue esto
 	private Integer combo;
 	
 	public Raza getRaza() {
